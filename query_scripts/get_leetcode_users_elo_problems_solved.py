8907812c827e6f7889676589e4230fa1404b8b12
RaymondJones <Rayjones2170@gmail.com>
2024-10-18 11:10:15 -0500

    add new users and update way of adding new users when problem count is zero

--- query_scripts/get_leetcode_users_elo_problems_solved.py
+++ query_scripts/get_leetcode_users_elo_problems_solved.py
@@ -2,20 +2,23 @@ import requests
 import sys
 import json
 from cookies import cookies
+
 # Define the headers and cookies as given in your template
 
 
 headers = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-    'Accept-Language': 'en-US,en;q=0.5',
-    'Connection': 'keep-alive',
-    'Sec-Fetch-Dest': 'document',
-    'Sec-Fetch-Mode': 'navigate',
-    'Sec-Fetch-Site': 'none',
-    'Sec-Fetch-User': '?1',
-    'Upgrade-Insecure-Requests': '1',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:123.0) Gecko/20100101 Firefox/123.0'
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Connection": "keep-alive",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-User": "?1",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:123.0) Gecko/20100101 Firefox/123.0",
 }
+
+
 def get_problems_solved(username):
     url = "https://leetcode.com/graphql"
     payload = {
@@ -40,16 +43,21 @@ def get_problems_solved(username):
             }
         }
         """,
-        "variables": {"username": username}
+        "variables": {"username": username},
     }
     response = requests.post(url, headers=headers, cookies=cookies, json=payload)
     if response.status_code == 200:
         data = response.json()
-        total_problems_solved = data['data']['matchedUser']['submitStatsGlobal']['acSubmissionNum'][0]['count']
+        total_problems_solved = data["data"]["matchedUser"]["submitStatsGlobal"][
+            "acSubmissionNum"
+        ][0]["count"]
         return total_problems_solved
     else:
-        print(f'Failed to retrieve problem stats for {username}: {response.status_code}')
+        print(
+            f"Failed to retrieve problem stats for {username}: {response.status_code}"
+        )
         return None
+
 
 def get_elo_of_leetcoder(username):
     url = "https://leetcode.com/graphql"
@@ -62,38 +70,44 @@ def get_elo_of_leetcoder(username):
             }
         }
         """,
-        "variables": {"username": username}
+        "variables": {"username": username},
     }
     response = requests.post(url, headers=headers, cookies=cookies, json=payload)
     if response.status_code == 200:
         data = response.json()
-        return data['data']['userContestRanking']['rating']
+        return data["data"]["userContestRanking"]["rating"]
     else:
-        print('Failed to retrieve data for {}: {}'.format(username, response.status_code))
+        print(
+            "Failed to retrieve data for {}: {}".format(username, response.status_code)
+        )
         return None
 
+
 def load_existing_elos(filename):
-    with open(filename, 'r') as file:
+    with open(filename, "r") as file:
         return json.load(file)
+
 
 def update_json(filename, users):
     data = []
     for user in users:
-        data.append({
-            "name": user['name'],
-            "elo": user['elo'],
-            "prev_elo": user.get("prev_elo", 0),
-            'prev_problem_count': user['prev_problem_count'],
-            'current_problem_delta': user['current_problem_delta'],
-            'problems_each_week': user.get("problems_each_week", []),
-            'current_problem_count': user['current_problem_count']
-        })
-    with open(filename, 'w') as file:
+        data.append(
+            {
+                "name": user["name"],
+                "elo": user["elo"],
+                "prev_elo": user.get("prev_elo", 0),
+                "prev_problem_count": user["prev_problem_count"],
+                "current_problem_delta": user["current_problem_delta"],
+                "problems_each_week": user.get("problems_each_week", []),
+                "current_problem_count": user["current_problem_count"],
+            }
+        )
+    with open(filename, "w") as file:
         json.dump(data, file, indent=4)
 
 
 def read_usernames_from_file(filename):
-    with open(filename, 'r') as file:
+    with open(filename, "r") as file:
         return [line.strip() for line in file.readlines()]
 
 
@@ -102,7 +116,7 @@ def write_elos_to_json(filename, user_elos):
     for user, elo, prev_elo in user_elos:
         data.append({"name": user, "elo": elo, "prev_elo": prev_elo})
 
-    with open(filename, 'w') as file:
+    with open(filename, "w") as file:
         json.dump(data, file, indent=4)
 
 
@@ -113,10 +127,16 @@ def daily_update(existing_users):
         problems_solved_count = get_problems_solved(username)
         if problems_solved_count:
             print("COUNT WAS", problems_solved_count)
+            old_problems_count = user.get("prev_problem_count")
             user["current_problem_count"] = problems_solved_count
-            user["current_problem_delta"] = problems_solved_count - user.get("prev_problem_count", 0)
+            if old_problems_count == 0:
+                user["current_problem_delta"] = 0
+            else:
+                user["current_problem_delta"] = problems_solved_count - user.get(
+                    "prev_problem_count", problems_solved_count
+                )
             print("Problems solved by user...", problems_solved_count)
-    update_json('../leetcode-elo/public/users_by_elo.json', existing_users)
+    update_json("../leetcode-elo/public/users_by_elo.json", existing_users)
     pass
 
 
@@ -132,20 +152,25 @@ def weekly_update(existing_users):
             else:
                 user["problems_each_week"] = [user.get("current_problem_count", 0)]
 
-            user["prev_problem_count"] = user.get("current_problem_count", 0)
+            user["prev_problem_count"] = user.get(
+                "current_problem_count", problems_solved_count
+            )
             user["current_problem_count"] = problems_solved_count
-            user["current_problem_delta"] = problems_solved_count - user.get("prev_problem_count", 0)
+            user["current_problem_delta"] = problems_solved_count - user.get(
+                "prev_problem_count", problems_solved_count
+            )
             print("Problems solved by user...", problems_solved_count)
-    update_json('../leetcode-elo/public/users_by_elo.json', existing_users)
+    update_json("../leetcode-elo/public/users_by_elo.json", existing_users)
 
 
 def main(weekly_or_daily):
-    existing_users = load_existing_elos('../leetcode-elo/public/users_by_elo.json')
+    existing_users = load_existing_elos("../leetcode-elo/public/users_by_elo.json")
     if weekly_or_daily == "weekly":
         weekly_update(existing_users)
     elif weekly_or_daily == "daily":
         daily_update(existing_users)
     print("finished..")
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
